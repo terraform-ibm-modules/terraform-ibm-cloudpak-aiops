cbb5d384dfddc96093995d01b6dc349b0d81b1b6
jgod1360 <--list>
2022-06-23 15:24:17 -0400

    update cp4aiops

--- versions.tf
+++ versions.tf
@@ -1,30 +1,20 @@
 #####################################################
-# activity tracker as a service
-# Copyright 2020 IBM
+# Cloud Pak for CP4AIOPS
+# Copyright 2022 IBM
 #####################################################
 
-/***************************************************
-NOTE: To source a particular version of IBM terraform
-provider version, configure the parameter `version`.
-
 terraform {
-  required_version = ">=0.13"
+  required_version = ">= 0.13"
   required_providers {
     ibm = {
-      source = "IBM-Cloud/ibm"
-      version = "1.20.0"
+      source  = "ibm-cloud/ibm"
+      version = "1.34"
     }
-  }
-}
-If we dont configure the version parameter,
-it fetches latest provider version.
-****************************************************/
-
-terraform {
-  required_version = ">=0.13"
-  required_providers {
-    ibm = {
-      source = "IBM-Cloud/ibm"
+    external = {
+      source = "hashicorp/external"
+    }
+    null = {
+      source = "hashicorp/null"
     }
   }
 }

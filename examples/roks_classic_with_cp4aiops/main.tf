c238131fbe7adb74f72520ae31bfebf009688403
jgod1360 <--list>
2022-06-29 12:49:35 -0400

    Update main.tf

--- examples/roks_classic_with_cp4aiops/main.tf
+++ examples/roks_classic_with_cp4aiops/main.tf
@@ -2,11 +2,11 @@ provider "ibm" {
   region = var.region
 }
 
-resource "random_string" "this" {
-  length  = 6
-  special = false
-  upper   = false
-}
+//resource "random_string" "this" {
+//  length  = 6
+//  special = false
+//  upper   = false
+//}
 
 data "ibm_resource_group" "rg" {
   name = var.resource_group
